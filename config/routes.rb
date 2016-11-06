ff7e769df57cb8ad8d3718e6ee94313047f07c64
Luke Thomas <luke.thomas@atlasobscura.com>
2016-11-06 16:51:48 -0500

    Syntax error in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Redirect all herokuapp domains
-	constraints lambda { |req| req.env['HTTP_HOST'].include?("herokuapp") do
+	constraints lambda { |req| req.env['HTTP_HOST'].include?("herokuapp") } do
 	  match '/(*path)' => redirect { |params, req|
 	    "//#{SITE_HOST}#{req.fullpath}"
 	  }, via: [:get, :post]
