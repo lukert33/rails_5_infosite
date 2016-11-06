51293c10c3b138a872040a7c247511829a374663
Luke Thomas <luke.thomas@atlasobscura.com>
2016-11-06 16:49:06 -0500

    Redirect within app to a non .herokuapp domain.

--- config/initializers/01Constants.rb
+++ config/initializers/01Constants.rb
@@ -1,2 +1,9 @@
 SITE_TITLE = "Lead and Galvanized Pipe Inspection"
 FIRM_TITLE = "Residential Lead Water Testing, LLC"
+
+SITE_HOSTS = {
+  "development" => "localhost:3000",
+  "production" =>  "www.leadpipetesting.com"
+}
+
+SITE_HOST = SITE_HOSTS[Rails.env]
