38ec57ae89577ec84a5e4e1b17c4a732b8f0f2b8
Luke Thomas <lukert33@gmail.com>
2017-04-09 16:24:23 -0400

    Direct to the info inbox.

--- app/mailers/contact_request_mailer.rb
+++ app/mailers/contact_request_mailer.rb
@@ -1,9 +1,9 @@
 class ContactRequestMailer < ApplicationMailer
+  default to: "info@leadpipetesting.com"
   def info_request(args)
     @user_email = args[:user_email]
     @body_lines = args[:body_lines]
     mail(
-      to: "lukert33@gmail.com",
       subject: args[:subject]
     )
   end
